2f6d05bcfdd0ab4731a796f6cb9cf4d830f19b77
arowent <arowent@mail.ru>
2022-08-04 23:12:41 +0300

    Update files

--- autolinks.sh
+++ autolinks.sh
@@ -9,47 +9,19 @@ function setLinks() {
       if [ -d $where_dir$(basename $item) ]; then
         rm $where_dir$(basename $item)
         ln -s $item /home/arowent/example/stend/
+        echo "Ссылка на папку $where_dir$(basename $item) - перезаписана."
       else
         ln -s $item /home/arowent/example/stend/
+        echo "Ссылка на папку $where_dir$(basename $item) - создана."
       fi
     fi
   done
 }
 
+# run script
+echo "This script does not require superuser access."
 if [ $1 ]; then
   setLinks
 else
-  echo "Write directory"
+  echo "Вы не указали папку из которой требуется создать smlink."
 fi
-
-#for item in /home/arowent/example/project/*; do
-#  if [ -d $item ]; then
-#    if [ -d /home/arowent/example/stend/$(basename $item) ]; then
-#      #      echo $(basename $item)
-#      rmdir /home/arowent/example/stend/$(basename $item)
-#      ln -s $item /home/arowent/example/stend/
-#    else
-#      ln -s $item /home/arowent/example/stend/
-#    fi
-#  fi
-#done
-
-#where_dir=$1
-#from_dir=$(ls $2)
-#group=$(groups $(whoami))
-#
-#echo $group
-
-# # echo "Where directory: $(ls $1)"
-# # echo "From directory: $(ls $2)"
-
-# for item in $from_dir; do
-#   echo "$2$item"
-#   ln -s "$1" "$2$item"
-#   chown $(whoami):
-#   # ln -s
-#   # if [ -d $item ]; then
-#   #   echo '$item'
-#   #   # ln -s $1 $2
-#   # fi
-# done
