bc28a7ffb624ffb3338c44e9887a2fb01840019f
an.lopatko <an.lopatko@tensor.ru>
2022-08-09 12:18:52 +0300

    update

---
+++ autolinks.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+from os import listdir
+from os.path import isdir
+
+def main(args):
+    if args.source and args.receiver:
+        for file in listdir(args.source):
+            file_path = args.source + '\\' + file
+            if isdir(file_path):
+                try:
+                    os.symlink(file_path, args.receiver + '\\' + file)
+                    print('Created symlink from folder \'{}\''.format(file))
+                except FileExistsError:
+                    os.rmdir(args.receiver + '\\' + file)
+                    os.symlink(file_path, args.receiver + '\\' + file)
+                    print('Overwrite symlink from folder \'{}\''.format(file))
+                except FileNotFoundError:
+                    print('Specify the existing directory paths')
+                    break
+    else:
+        print('Specify the paths for the program to work')
+        
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Command Line Autolinks')
+    parser.add_argument('-s', '--source', help='The directory from where we copy the folders')
+    parser.add_argument('-r', '--receiver', help='The directory that accepts files')
+    args = parser.parse_args()
+    main(args)
